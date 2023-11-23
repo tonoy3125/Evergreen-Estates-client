405a4e9adb7c0bfa756ff5f7bdf83e0bd5af5850
tonoy3125 <shaifshajedt@gmail.com>
2023-11-23 21:15:57 +0600

    folder created commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Root from "../Layout/Root/Root";
+import ErrorElement from "../Layout/ErrorElement/ErrorElement";
+import Home from "../Pages/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorElement></ErrorElement>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+export default router
