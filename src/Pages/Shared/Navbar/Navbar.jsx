65629ccc9a7a0b73335cb33f5930356c7604215f
tonoy3125 <shaifshajedt@gmail.com>
2023-11-24 18:11:10 +0600

    user login and logout toggle added commit

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import { AiOutlineMenu } from 'react-icons/ai';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from "../../../assets/logo.png";
+import useAuth from './../../../Hooks/useAuth';
 
 const Navbar = () => {
+    const { user, logOut } = useAuth()
+
+
+    const handleLogOut = () => {
+        logOut()
+            .then(result => {
+                console.log(result.user)
+            })
+            .then(error => {
+                console.log(error)
+            })
+    }
+
     return (
         <div className="">
             <div className="navbar  container mx-auto">
@@ -42,8 +56,8 @@ const Navbar = () => {
                         <li className="text-white text-lg hover:text-[#46d993] font-medium"><NavLink className={({ isActive, isPending }) =>
                             isPending ? "pending" : isActive ? "font-bold text-gray-50 underline" : ""
                         } to="/postedjobs">Dashboard</NavLink></li>
-                        
-                       
+
+
 
 
                     </ul>
@@ -52,7 +66,34 @@ const Navbar = () => {
                 {/* User login and logout toggle */}
 
                 <div className="navbar-end flex items-center gap-5">
+                    {
+                        user ? <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar tooltip" >
+                                <div className="w-10 h-10 rounded-full">
+                                    <img src={user.photoURL} />
+                                </div>
+                            </label>
 
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg text-black font-bold bg-base-100 rounded-box w-52">
+                                <li className="mb-1">
+                                    <button className="px-5 py-3 rounded-lg bg-[#6a4c93] text-white">{user.displayName}</button>
+
+                                </li>
+                                <li>
+                                    <button onClick={handleLogOut} className="px-5 py-3 rounded-lg bg-[#6a4c93] text-white">SignOut</button>
+
+                                </li>
+                            </ul>
+                        </div>
+                            :
+                            <Link to="/login">
+                                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img src="https://i.ibb.co/WsLGC0q/user.png" />
+                                    </div>
+                                </label>
+                            </Link>
+                    }
                 </div>
             </div>
         </div>
