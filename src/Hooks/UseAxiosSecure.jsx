2d47cab9f16eb819e3e4999acf34e799eb4fc375
tonoy3125 <shaifshajedt@gmail.com>
2023-11-25 19:20:14 +0600

    axios secure hook added commit

---
+++ src/Hooks/UseAxiosSecure.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+const UseAxiosSecure = () => {
+    return axiosSecure
+};
+
+export default UseAxiosSecure;
