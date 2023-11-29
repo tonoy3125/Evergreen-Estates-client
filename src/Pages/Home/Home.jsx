608d39db2a93972a31ab1dd1b1918525f1f11063
tonoy3125 <shaifshajedt@gmail.com>
2023-11-30 00:16:58 +0600

    our service section added commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import AdvertisementCard from "../Advertisement/AdvertisementCard";
 import Banner from "../Banner/Banner";
 import Reviews from "../Reviews/Reviews";
+import OurServices from "../Our Services/OurServices";
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <Helmet><title>Evergreen Estates | Home</title></Helmet>
             <AdvertisementCard></AdvertisementCard>
+            <OurServices></OurServices>
             <Reviews></Reviews>
         </div>
     );
---
+++ src/Pages/Our Services/OurServices.jsx
@@ -0,0 +1,114 @@
+
+
+const OurServices = () => {
+    return (
+        <div>
+            <section className=" dark:bg-gray-900">
+                <div className="container px-6 py-10 mx-auto">
+                    <h1 className="text-5xl font-bold text-center text-white">Our Services</h1>
+
+                    <p className="mt-4 text-white  xl:mt-6 text-center ">
+                        Top real estate services available
+                    </p>
+
+                    <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+                        <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                            <img src="https://i.ibb.co/B4cqvSd/icons8-home-1.gif" alt="" />
+
+                            <h1 className="text-xl font-semibold text-white capitalize dark:text-white">Sell your home</h1>
+
+                            <p className="text-white dark:text-gray-300">
+                                We sell your home at the best market price.
+                            </p>
+
+                            <a href="#" className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                            <img src="https://i.ibb.co/PzsZzYB/icons8-loan-64.png" alt="" />
+
+                            <h1 className="text-xl font-semibold text-white capitalize dark:text-white">Home loans</h1>
+
+                            <p className="text-white dark:text-gray-300">
+                                We offer you free consultancy to get a loan.
+                            </p>
+
+                            <a href="#" className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                            <img className="ml-10" src="https://i.ibb.co/VL1jHWK/icons8-scales.gif" alt="" />
+
+                            <h1 className="text-xl font-semibold text-white capitalize dark:text-white">Legal services</h1>
+
+                            <p className="text-white dark:text-gray-300">
+                                Expert legal help for all related property items.
+                            </p>
+
+                            <a href="#" className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                            <img src="https://i.ibb.co/GRHjRDm/icons8-search.gif" alt="" />
+
+                            <h1 className="text-xl font-semibold text-white capitalize dark:text-white">Home inspection</h1>
+
+                            <p className="text-white dark:text-gray-300">
+                                We make sure you get what you were promised.
+                            </p>
+
+                            <a href="#" className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                            <img className="ml-10" src="https://i.ibb.co/VQJWV84/Animation-1701278973169.gif" alt="" />
+
+                            <h1 className="text-xl font-semibold text-white capitalize dark:text-white">Evaluation</h1>
+
+                            <p className="text-white dark:text-gray-300">
+                            We offer you free evaluation to get a mortgage loan.
+                            </p>
+
+                            <a href="#" className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                            <img className="ml-10" src="https://i.ibb.co/VQJWV84/Animation-1701278973169.gif" alt="" />
+
+                            <h1 className="text-xl font-semibold text-white capitalize dark:text-white">Photoshoot</h1>
+
+                            <p className="text-white dark:text-gray-300">
+                            We prepare your home visual presentation.
+                            </p>
+
+                            <a href="#" className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </section >
+        </div >
+    );
+};
+
+export default OurServices;
