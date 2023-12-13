b40786934a3303ede672eb9d58817e1f7f88bca6
tonoy3125 <shaifshajedt@gmail.com>
2023-12-13 16:40:06 +0600

    root loading state added commit

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,16 +1,46 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
 import Footer from "../../Pages/Shared/Footer/Footer";
+import { useEffect, useState } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 
 const Root = () => {
     const location = useLocation()
+    const [loading, setLoading] = useState(true);
     const noHeaderFooter = location.pathname.includes('/login') || location.pathname.includes('/register')
+
+
+    useEffect(() => {
+        // Simulate a loading delay (replace with your actual data fetching logic)
+        const delay = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+
+        // Cleanup the timeout to avoid memory leaks
+        return () => clearTimeout(delay);
+    }, []); // Run only once on component mount
+
     return (
         <div>
-            {noHeaderFooter || <Navbar></Navbar>}
-            <Outlet></Outlet>
-            {noHeaderFooter || <Footer></Footer>}
+
+            {loading ? (
+                <div>
+                    <Player
+                        autoplay
+                        loop
+                        src="https://lottie.host/cea9212d-8f86-4096-a3d5-b5c8839ba977/h60Gsnco7e.json"
+
+                    >
+                    </Player>
+                </div>
+            ) : (
+                <>
+                    {noHeaderFooter || <Navbar />}
+                    <Outlet />
+                    {noHeaderFooter || <Footer />}
+                </>
+            )}
         </div>
     );
 };
