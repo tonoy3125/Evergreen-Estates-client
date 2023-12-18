399dd48e4b245bc1aad3dec5fa4906d2f968e427
tonoy3125 <shaifshajedt@gmail.com>
2023-12-18 16:05:01 +0600

    admin profile new card added commit

--- src/DashBoard/Admin/AdminProfile/AdminProfile.jsx
+++ src/DashBoard/Admin/AdminProfile/AdminProfile.jsx
@@ -6,6 +6,7 @@ import { FaMoneyCheckDollar } from "react-icons/fa6";
 import { AiTwotonePropertySafety } from "react-icons/ai";
 import { FaUsers } from "react-icons/fa";
 import { GoCodeReview } from "react-icons/go";
+import { MdSell } from "react-icons/md";
 
 
 const AdminProfile = () => {
@@ -60,9 +61,22 @@ const AdminProfile = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                         </div>
                     </div>
-                    <div className="stat-title text-white">Total Brought Properties</div>
+                    <div className="stat-title text-white">Total Properties</div>
                     <div className="flex items-center justify-start gap-5">
                         <AiTwotonePropertySafety className="text-white text-3xl" />
+                        <div className="stat-value text-white">{stats?.properties}</div>
+                    </div>
+                    <div className="stat-desc text-white">21% more than last month</div>
+                </div>
+                <div className="stat border border-white rounded-lg shadow-2xl  shadow-gray-300/50">
+                    <div className="stat-figure text-white">
+                        <div className="stat-figure text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                        </div>
+                    </div>
+                    <div className="stat-title text-white">Total Brought Properties</div>
+                    <div className="flex items-center justify-start gap-5">
+                        <MdSell className="text-white text-3xl" />
                         <div className="stat-value text-white">{stats?.propertyBrought}</div>
                     </div>
                     <div className="stat-desc text-white">21% more than last month</div>
